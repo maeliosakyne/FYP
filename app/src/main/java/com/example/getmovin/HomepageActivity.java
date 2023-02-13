6ac5f23fe9068e9befa2778bebc49bd05a1cac1d
MAELIOSA KYNE <19232624@studentmail.ul.ie>
2023-02-13 18:05:26 +0000

    Project files with new xml screens recovered

---
+++ app/src/main/java/com/example/getmovin/HomepageActivity.java
@@ -0,0 +1,18 @@
+package com.example.fypapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class HomepageActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+    }
+
+}
