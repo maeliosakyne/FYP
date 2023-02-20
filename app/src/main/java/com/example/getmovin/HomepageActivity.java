15150c5368a8ee96a8c9660117f5901b06063bb8
MAELIOSA KYNE <19232624@studentmail.ul.ie>
2023-02-20 16:59:55 +0000

    Java classes added for app buttons and youtube video functionality

--- app/src/main/java/com/example/getmovin/HomepageActivity.java
+++ app/src/main/java/com/example/getmovin/HomepageActivity.java
@@ -1,12 +1,16 @@
-package com.example.fypapp;
+package com.example.getmovin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class HomepageActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,13 @@ public class HomepageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
 
     }
-
+    public void onClickPracticeExercises(View view) {
+        startActivity(new Intent(HomepageActivity.this, PracticeExercises.class));
+    }
+    public void onClickHelp(View view) {
+        startActivity(new Intent(HomepageActivity.this, HelpActivity.class));
+    }
+    public void Completed(View view) {
+        startActivity(new Intent(HomepageActivity.this, CompletedExercises.class));
+    }
 }
